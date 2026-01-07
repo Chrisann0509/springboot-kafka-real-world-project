2b7a574a05b67795494896480ecf7f000da3ffe2
chrisann.lee <chrisannlee97@gmail.com>
2026-01-07 16:23:21 +0800

    Kafka Real-World Project:Kafka Consumer Implementation

---
+++ kafka-consumer-database/src/main/java/net/javaguides/springboot/KafkaDatabaseConsumer.java
@@ -0,0 +1,16 @@
+package net.javaguides.springboot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaDatabaseConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consumer(String eventMessage) {
+        LOGGER.info(String.format("Event message received -> %s", eventMessage));
+    }
+}
